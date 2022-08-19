42d3d43cdac5f4e5afdffc3f00ed7b2ee553fa46
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-08-20 01:14:50 +0800

    Removed some white space

--- CS50P - Lecture 0 - Functions, Variables/CS50P - Lecture 0 - Functions, Variables.py
+++ CS50P - Lecture 0 - Functions, Variables/CS50P - Lecture 0 - Functions, Variables.py
@@ -4,7 +4,7 @@
 name = input("What's your name? ")
 # Say hello to user
 print(f'hello, {name}')
-print('hello, ' + name) # The "+" appends the following variable to the former one
+print('hello,' + name) # The "+" appends the following variable to the former one
 print('hello,', name) # The "," acts the same as the "+", but adds a space before the new variable
 
 
